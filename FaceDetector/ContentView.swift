f9da0d93c3d25541d021aae95533426658d81d94
Amani <amanikhunter@gmail.com>
2020-02-28 18:11:00 -0500

    Updates

--- FaceDetector/ContentView.swift
+++ FaceDetector/ContentView.swift
@@ -7,15 +7,123 @@
 //
 
 import SwiftUI
+import Vision
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var imagePickerOpen: Bool = false
+    @State private var cameraOpen: Bool = false
+    @State private var image: UIImage? = nil
+    @State private var faces: [VNFaceObservation]? = nil
+    
+    // BEGIN FD_starter_cv_2_a
+    private var faceCount: Int { return faces?.count ?? 0 }
+    private let placeholderImage = UIImage(named: "placeholder")!
+    
+    private var cameraEnabled: Bool {
+        UIImagePickerController.isSourceTypeAvailable(.camera)
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
+    private var detectionEnabled: Bool { image != nil && faces == nil }
+    // END FD_starter_cv_2_a
+    
+    // BEGIN FD_starter_cv_2_b
+    var body: some View {
+        if imagePickerOpen { return imagePickerView() }
+        if cameraOpen { return cameraView() }
+        return mainView()
+    }
+    // END FD_starter_cv_2_b
+    
+    // BEGIN FD_starter_cv_2_c
+    private func getFaces() {
+        print("Getting faces...")
+        self.faces = []
+        self.image?.detectFaces { result in
+            self.faces = result
+            
+            if let image = self.image, let annotatedImage = result?.drawnOn(image) {
+                self.image = annotatedImage
+            }
+        }
+    }
+    // END FD_starter_cv_2_c
+    
+    // BEGIN FD_starter_cv_2_d
+    private func controlReturned(image: UIImage?) {
+        print("Image return \(image == nil ? "failure" : "success")...")
+        self.image = image?.fixOrientation()
+        self.faces = nil
+    }
+    // END FD_starter_cv_2_d
+    
+    // BEGIN FD_starter_cv_2_e
+    private func summonImagePicker() {
+        print("Summoning ImagePicker...")
+        imagePickerOpen = true
+    }
+    // END FD_starter_cv_2_e
+    
+    // BEGIN FD_starter_cv_2_f
+    private func summonCamera() {
+        print("Summoning camera...")
+        cameraOpen = true
+    }
+    // END FD_starter_cv_2_f
+}
+// END FD_starter_cv_2
+
+// BEGIN FD_starter_cv_ext_1
+extension ContentView {
+    // BEGIN FD_starter_cv_ext_1_a
+    private func mainView() -> AnyView {
+        return AnyView(NavigationView {
+            MainView(
+                image: image ?? placeholderImage,
+                text: "\(faceCount) face\(faceCount == 1 ? "" : "s")") {
+                    TwoStateButton(
+                        text: "Detect Faces",
+                        disabled: !detectionEnabled,
+                        action: getFaces
+                    )
+            }
+            .padding()
+            .navigationBarTitle(Text("FDDemo"), displayMode: .inline)
+            .navigationBarItems(
+                leading: Button(action: summonImagePicker) {
+                    Text("Select")
+                },
+                trailing: Button(action: summonCamera) {
+                    Image(systemName: "camera")
+                }.disabled(!cameraEnabled)
+            )
+        })
+    }
+    // END FD_starter_cv_ext_1_a
+    
+    // BEGIN FD_starter_cv_ext_1_b
+    private func imagePickerView() -> AnyView {
+        return  AnyView(ImagePicker { result in
+            self.controlReturned(image: result)
+            self.imagePickerOpen = false
+        })
+    }
+    // END FD_starter_cv_ext_1_b
+    
+    // BEGIN FD_starter_cv_ext_1_c
+    private func cameraView() -> AnyView {
+        return  AnyView(ImagePicker(camera: true) { result in
+            self.controlReturned(image: result)
+            self.cameraOpen = false
+        })
+    }
+    // END FD_starter_cv_ext_1_c
+}
+// END FD_starter_cv_ext_1
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
---
+++ FaceDetector/Faces.swift
@@ -0,0 +1,65 @@
+//
+//  Faces.swift
+//  FaceDetector
+//
+//  Created by Amani Hunter on 2/28/20.
+//  Copyright © 2020 INAMA. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+extension UIImage {
+    func detectFaces(completion: @escaping ([VNFaceObservation]?) -> ()) {
+        
+        guard let image = self.cgImage else { return completion(nil) }
+        let request = VNDetectFaceRectanglesRequest()
+        
+        DispatchQueue.global().async {
+            let handler = VNImageRequestHandler(
+                cgImage: image,
+                orientation: self.cgImageOrientation
+            )
+
+            try? handler.perform([request])
+            
+            guard let observations =
+                request.results as? [VNFaceObservation] else {
+                    return completion(nil)
+            }
+
+            completion(observations)
+        }
+    }
+}
+extension Collection where Element == VNFaceObservation{
+    func drawnOn(_ image: UIImage) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(image.size, false, 1.0)
+        
+        guard let context = UIGraphicsGetCurrentContext() else{
+            return nil
+        }
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.setLineWidth(0.01 * image.size.width)
+        
+        let transform = CGAffineTransform(scaleX: 1, y: -1)
+            .translatedBy(x: 0, y: -image.size.height)
+        
+        for observation in self {
+            let rect = observation.boundingBox
+            
+            let normalizedRect = VNImageRectForNormalizedRect(rect, Int(image.size.width), Int(image.size.height)).applying(transform)
+            
+            context.stroke(normalizedRect)
+        }
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+    
+    
+}
